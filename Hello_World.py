37f3274fc2f44664f095c0a5ce84bb8c01babbe8
Zachary Jones <zrjones77@gmail.com>
2019-09-30 15:10:39 -0400

    Have the fitbit api working in python. Next steps are to sift through the data and figure out what is useful and how to use it

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,50 @@
-print("Hello World")
+import fitbit
+import gather_keys_oauth2 as Oauth2
+import pandas as pd
+import datetime
+
+server = Oauth2.OAuth2Server('22DFYT', 'd4a39b4a3f5e1c8ab789d4bec17c5d23')
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit('22DFYT', 'd4a39b4a3f5e1c8ab789d4bec17c5d23', oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1sec')
+
+
+########################################################################
+time_list = []
+val_list = []
+for i in fit_statsHR['activities-heart-intraday']['dataset']:
+    val_list.append(i['value'])
+    time_list.append(i['time'])
+heartdf = pd.DataFrame({'Heart Rate':val_list,'Time':time_list})
+
+heartdf.to_csv('heart'+ \
+               yesterday+'.csv', \
+               columns=['Time','Heart Rate'], header=True, \
+               index = False)
+
+
+#####################################################################
+"""Sleep data on the night of ...."""
+fit_statsSl = auth2_client.sleep(date='today')
+stime_list = []
+sval_list = []
+for i in fit_statsSl['sleep'][0]['minuteData']:
+    stime_list.append(i['dateTime'])
+    sval_list.append(i['value'])
+sleepdf = pd.DataFrame({'State':sval_list,
+                     'Time':stime_list})
+print(fit_statsSl)
+for j in fit_statsSl['summary'].keys():
+    print(j, " : ", fit_statsSl['summary'][j])
+sleepdf['Interpreted'] = sleepdf['State'].map({'2':'Awake','3':'Very Awake','1':'Asleep'})
+sleepdf.to_csv('sleep' + \
+               today+'.csv', \
+               columns = ['Time','State','Interpreted'],header=True,
+               index = False)
